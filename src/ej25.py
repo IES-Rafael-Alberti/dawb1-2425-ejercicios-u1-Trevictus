2d2d7d09c34fb674a24dbef2b85117d789b0377b
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-09-29 15:12:20 +0200

    Ejercicios del 22-27

---
+++ src/ej25.py
@@ -0,0 +1,13 @@
+#Ejercicio 1.2.25
+"""Escribir un programa que pregunte al usuario la fecha de su nacimiento en formato dd/mm/aaaa y muestra por pantalla, el día, el mes y el año. Adaptar el programa anterior para que también funcione cuando el día o el mes se introduzcan con un solo carácter.
+
+"""
+
+def mostrar_fecha():
+    fecha = (input("Ingresa tu fecha de nacimiento en el formato dd/mm/aaaa: "))
+
+    dia, mes, anio = fecha.split("/")
+
+    print(f"Nació un {dia} del mes {mes} del año {anio}.")
+
+mostrar_fecha()
