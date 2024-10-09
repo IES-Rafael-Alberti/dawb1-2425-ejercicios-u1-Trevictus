c5d431bc2ea8e1321b13dd7dadfbc56e519a5317
vgomtej353 <gtvictor@iesrafaelalberti.es>
2024-10-09 18:10:05 +0200

    p1.2e acabado por completo

--- src/ej04_def2.py
+++ src/ej04_def2.py
@@ -1,11 +1,11 @@
-def conversion_celsius():
-    farenheits = round(float(input("Introduce que tempratura hace en gº Farenheits, redondeado a dos decimales: ")),2)
+def conversion_celsius(farenheits):
     celsius = (farenheits - 32) * (5 / 9)
 
     return "La temperatura en Celsius es {:.2f}ºC ({:.2f})".format(celsius, farenheits)
 
 def main():
-    print(conversion_celsius())
+    farenheits = float(input("Introduce que tempratura hace en gº Farenheits, redondeado a dos decimales: "))
+    print(conversion_celsius(farenheits))
 
 if __name__ == "__main__":
     main()
